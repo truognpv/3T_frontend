3113f1c5a9322b1eda051a3838faf965b138a3bc
lielove <truongpv328@gmail.com>
2022-03-30 22:47:39 +0700

    header

--- src/components/Layout/Header/Info.vue
+++ src/components/Layout/Header/Info.vue
@@ -31,9 +31,55 @@ const mouseoverList = ref(false)
         </router-link>
         <router-link to="ifo"><h1>IFO</h1></router-link>
         <router-link to="voting"><h1>Voting</h1> </router-link>
-        <router-link to="leaderboard"><h1>Leaderboard</h1> </router-link>
-        <a href="#"><h1>Blog</h1> </a>
-        <a href="#"><h1 class="hover:rounded-b-2xl">Docs</h1> </a>
+        <router-link to="leaderboard"
+          ><h1 class="border-y">Leaderboard</h1>
+        </router-link>
+        <a href="#"
+          ><h1 class="flex justify-between">
+            Blog
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              class="icon icon-tabler icon-tabler-logout"
+              width="24"
+              height="24"
+              viewBox="0 0 24 24"
+              stroke-width="1.5"
+              stroke="#9e9e9e"
+              fill="none"
+              stroke-linecap="round"
+              stroke-linejoin="round"
+            >
+              <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+              <path
+                d="M14 8v-2a2 2 0 0 0 -2 -2h-7a2 2 0 0 0 -2 2v12a2 2 0 0 0 2 2h7a2 2 0 0 0 2 -2v-2"
+              />
+              <path d="M7 12h14l-3 -3m0 6l3 -3" />
+            </svg>
+          </h1>
+        </a>
+        <a href="#"
+          ><h1 class="flex justify-between hover:rounded-b-2xl">
+            Docs
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              class="icon icon-tabler icon-tabler-logout"
+              width="24"
+              height="24"
+              viewBox="0 0 24 24"
+              stroke-width="1.5"
+              stroke="#9e9e9e"
+              fill="none"
+              stroke-linecap="round"
+              stroke-linejoin="round"
+            >
+              <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+              <path
+                d="M14 8v-2a2 2 0 0 0 -2 -2h-7a2 2 0 0 0 -2 2v12a2 2 0 0 0 2 2h7a2 2 0 0 0 2 -2v-2"
+              />
+              <path d="M7 12h14l-3 -3m0 6l3 -3" />
+            </svg>
+          </h1>
+        </a>
       </div>
     </div>
   </router-link>
--- src/components/Layout/Header/Trade.vue
+++ src/components/Layout/Header/Trade.vue
@@ -9,7 +9,31 @@ import TitleMenuList from './TitleMenuList.vue'
     </router-link>
     <router-link to="limit"><h1 class="page">Limit</h1></router-link>
     <router-link to="swap"><h1 class="page">Liquidity</h1> </router-link>
-    <a href="#"><h1 class="page hover:rounded-b-2xl">Perpetual</h1> </a>
+    <a href="#">
+      <div class="page hover:rounded-b-2xl">
+        <h1 class="flex items-center justify-between">
+          Perpetual
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            class="icon icon-tabler icon-tabler-logout"
+            width="24"
+            height="24"
+            viewBox="0 0 24 24"
+            stroke-width="1.5"
+            stroke="#9e9e9e"
+            fill="none"
+            stroke-linecap="round"
+            stroke-linejoin="round"
+          >
+            <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+            <path
+              d="M14 8v-2a2 2 0 0 0 -2 -2h-7a2 2 0 0 0 -2 2v12a2 2 0 0 0 2 2h7a2 2 0 0 0 2 -2v-2"
+            />
+            <path d="M7 12h14l-3 -3m0 6l3 -3" />
+          </svg>
+        </h1>
+      </div>
+    </a>
   </TitleMenuList>
 </template>
 
